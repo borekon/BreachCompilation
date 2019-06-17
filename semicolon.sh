26128e4edbb2215884bea048c309df1184954b40
borekon <vivancos@outlook.es>
2019-06-17 12:01:52 +0200

    Improved semicolon.sh
    Now only the files with semicolon separing mails and password are changed.

--- semicolon.sh
+++ semicolon.sh
@@ -2,6 +2,15 @@
 # Some files come with semicolon instead of colon, so the script sorter.sh gives an error and no data is recorded.
 # This script changes all the semicolons in a file to colons
 
-for i in inputbreach/*; do
-  sed -i 's/\;/\:/g' $i
+t=1;
+for i in inputbreach/*;
+	do if head -n1 -q $i | grep ";" >> /dev/null
+	then
+    echo "Found $i" ;
+    echo "Replacing...";
+    sed -i 's/\;/\:/g' $i ;
+    echo -e "Replaced $t files so far\n";
+    t=$((t+1));
+	#else echo "Not found";
+	fi
 done
