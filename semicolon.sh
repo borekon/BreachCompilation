d256e2fa650bd698b59172abf9bd84742a88848a
Alfonso <vivancos@outlook.es>
2019-08-07 09:12:23 +0200

    Update semicolon.sh
    Improved counters

--- semicolon.sh
+++ semicolon.sh
@@ -2,31 +2,31 @@
 # Some files come with semicolon or spaces instead of colon, so the script sorter.sh gives an error and no data is recorded.
 # This script changes all the semicolons in a file to colons
 
-t=1;
-o=1;
-p=1
+t=0;
+o=0;
+p=0;
 for i in inputbreach/*;
 do if head -n1 -q "$i" | grep ";" >> /dev/null
 then
     echo "Found $i" ;
     echo "Replacing...";
     sed -i 's/\;/\:/g' "$i" ;
-    echo -e "Replaced $t files so far\n";
     t=$((t+1));
+    echo -e "Replaced $t files so far\n";    
     elif head -n1 -q "$i" | grep "\ ">> /dev/null 
     then
 	    echo "Spaces in $i";
 	    echo "Replacing...";
 	    sed -i 's/\ /\:/g' "$i";
-	    echo -e "Replaced $o files so far\n";
 	    o=$((o+1));
-	     elif head -n1 -q "$i" | grep "," >> /dev/null
+	    echo -e "Replaced $o files so far\n";	    
+    elif head -n1 -q "$i" | grep "," >> /dev/null
     then
             echo "Comma found in $i";
             echo "Replacing...";
             sed -i 's/\,/\:/g' "$i";
-            echo -e "Replaced $p files so far\n";
-            p=$((p+1));
+	    p=$((p+1));
+            echo -e "Replaced $p files so far\n";            
     fi
 done
 echo "Replaced $t files with ;";
