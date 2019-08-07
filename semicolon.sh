26dee893bc7fb53e3edcb1fcd33485acc893fa19
Alfonso <vivancos@outlook.es>
2019-08-07 09:05:35 +0200

    Update semicolon.sh
    Included comma (,) replacement --> email@email.com,pass

--- semicolon.sh
+++ semicolon.sh
@@ -4,6 +4,7 @@
 
 t=1;
 o=1;
+p=1
 for i in inputbreach/*;
 do if head -n1 -q "$i" | grep ";" >> /dev/null
 then
@@ -19,6 +20,15 @@ then
 	    sed -i 's/\ /\:/g' "$i";
 	    echo -e "Replaced $o files so far\n";
 	    o=$((o+1));
-	
-fi
+	     elif head -n1 -q "$i" | grep "," >> /dev/null
+    then
+            echo "Comma found in $i";
+            echo "Replacing...";
+            sed -i 's/\,/\:/g' "$i";
+            echo -e "Replaced $p files so far\n";
+            p=$((p+1));
+    fi
 done
+echo "Replaced $t files with ;";
+echo "Replaced $o files with spaces";
+echo "Replaced $p files with commas";
